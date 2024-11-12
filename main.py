009c529dcfd00ccc10a9785c43bfd86462892e81
Kate Ramizovskaya <kr978407@gmail.com>
2024-11-12 11:03:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-search_str=input("Введите строку для поиска")
+search_str=input("Введите строку для поиска") #инициализация переменной, которая будет содержать строку для поиска
 
-with open('text.txt', 'r', encoding='utf-8') as file:
+with open('text.txt', 'r', encoding='utf-8') as file:#открытие файла для чтения
     lines=file.readlines()
 
-matching_lines=[]
-for line in lines:
-    if search_str in line:
+matching_lines=[]#инициализация переменной 
+for line in lines:#цикл for для перебора
+    if search_str in line:#оператор if для поиска подхождений
         matching_lines.append(line.strip())
 
-print(f"\nКолво найденных строк: {len(matching_lines)}")
+print(f"\nКолво найденных строк: {len(matching_lines)}")#вывод кол-ва найденных строк
 
-for line in matching_lines:
-    print(line)
+for line in matching_lines:#цикл for для вывода line
+    print(line)#вывод line
 
-sorted_lines=sorted(matching_lines, key=len)
+sorted_lines=sorted(matching_lines, key=len)#сортировка по длинне
 
-print("\n Отсортированные строки по длине: ")
-for line in sorted_lines:
-    print(line)
+print("\n Отсортированные строки по длине: ")#вывод строк
+for line in sorted_lines:#цикл for для вывода
+    print(line)#вывод строк
